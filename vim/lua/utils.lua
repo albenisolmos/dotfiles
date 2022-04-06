adf22037d849d113ebc12380b56fd17379293d2f
albenisolmos <albenisolmos88@gmail.com>
2022-04-06 13:52:41 -0400

    Fix: nill getting git root dir

--- vim/lua/utils.lua
+++ vim/lua/utils.lua
@@ -5,7 +5,10 @@ function M.telescope_project_search()
 		previewer = false,
 		shorten_path = true,
 		layout_strategy = "horizontal",
-		cwd = require('lspconfig.util').root_pattern(".git")(vim.fn.expand("%:p")),
+		cwd = require("plenary.job"):new({
+			command = "git",
+			args = { "rev-parse", "--show-toplevel" }
+		}):sync()[1]
 	}
 end
 
